3b04a2f3f852cab4121e20e1193f6f6ed2314583
darkster663 <emilp988@gmail.com>
2020-09-27 14:41:54 +0300

    lecture 226

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import BurgerBuilder from '../../containers/BurgerBuilder/BurgerBuilder';
+
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngridient/BurgerIngridient';
 
